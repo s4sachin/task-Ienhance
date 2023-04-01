3edabe9609412346e2b46182db3633605edd4c4e
s4sachin <sachinpurohit.text@gmail.com>
2023-04-01 14:42:43 +0530

    Project setup with API data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { DataService } from './service/data-service.service';
+import { DataInterface } from './interface/data';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'task-Ienhance';
+export class AppComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<DataInterface>();
+  csvRecords: DataInterface[] = [];
+  results: any;
+  displayedColumns: string[] = ['policyID', 'statecode', 'county', 'sum'];
+  @ViewChild(MatSort, { static: false })
+  sort!: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.onGetTableData();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onGetTableData() {
+    this.dataService
+      .getTableData()
+      .pipe()
+      .subscribe((response: string | any) => {
+        let lines = response.split('\n');
+        // console.log('Lines value is: ' + lines);
+        let headers = lines[0].split(',');
+        // console.log('headers value is: ' + headers);
+        let results: any[] = [];
+        for (let i = 1; i < lines.length; i++) {
+          let line = lines[i];
+          let row: { [index: string]: DataInterface } = {};
+          line.split(',').forEach((item: any, idx: number) => {
+            row[headers[idx]] = item;
+          });
+          results.push(row);
+        }
+        this.csvRecords = results;
+        this.dataSource.data = this.csvRecords;
+      });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { drive_v3 } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: drive_v3.Drive,
+      useFactory: (auth: OAuth2Client) => new drive_v3.Drive({ auth }),
+      deps: [OAuth2Client],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
