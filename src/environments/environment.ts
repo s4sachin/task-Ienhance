61a01a742b8bd8055c5d47f384f09572a6e881da
Sachin Purohit <sachinpurohit.text@gmail.com>
2023-04-01 16:52:18 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   CLIENT_ID:
-    '1073569547785-2f8ra9amr75m407dnnhhi2fjfdtn14n6.apps.googleusercontent.com',
-  CLIENT_SECRET: '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',
+    '',
+  CLIENT_SECRET: ''
   REFRESH_TOKEN:
-    '1//042TZswV_CvI_CgYIARAAGAQSNwF-L9IrTwKa6nyCgPOUA8mdrwrsDd05uMqnrzvPI06fjiyw5tjELCkNQi-p3A6FvFob_TSA5Jw',
-  csvFileId: '1FeAI_D3D_xlfZ__a7MFSCviBLrVc2jFk',
+    '',
+  csvFileId: '',
 };
